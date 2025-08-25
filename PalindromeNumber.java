57092013966767bdcdeb870ee4822a5716d211ae
yashvanthbalaji <333yashvanthbalaji@gmail.com>
2025-08-25 19:42:07 +0530

    Added Palindrome, Fibonacci, String Reverse, Number Reverse programs

---
+++ PalindromeNumber.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class PalindromeNumber 
+ {
+    public static void main ( String [ ]args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int orginal_num = number ;
+        int reversed = 0;
+        int digit = 0;
+        while (number !=0)
+        {
+
+           digit = number % 10 ;
+           reversed = reversed *10 + digit;
+           number = number / 10 ;
+            
+        }
+
+        if (orginal_num == reversed )
+        {
+            System.out.println("palindrome number ");
+        }
+        else
+        {
+
+            System.out.println(" NOT a Palindrome number " );
+            
+        }
+
+    }
+ }
