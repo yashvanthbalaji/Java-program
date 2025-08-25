c59d526f1fac22203a8d016f7cfdfab3749c2569
yashvanthbalaji <333yashvanthbalaji@gmail.com>
2025-08-25 21:47:01 +0530

    StringReverse.java

---
+++ StringReverse.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class StringReverse {
+    public static void main (String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the Word to be reversed");
+        String Word1 = scanner.nextLine();
+        String word2="";
+        for (int i = Word1.length()-1; i>=0 ; i--)
+        {
+            word2 = word2+Word1.charAt(i);
+        }
+        System.err.println("THE reversed string is :");
+        System.err.println(word2);
+    }
+    
+}
