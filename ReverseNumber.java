17f2b03dace6a247f352d182e9273fb8cacbeb10
yashvanthbalaji <333yashvanthbalaji@gmail.com>
2025-08-25 21:37:40 +0530

    ReverseNumber.java

---
+++ ReverseNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ReverseNumber 
+ {
+    public static void main ( String [ ]args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int orginal_num = number ;
+        int reversed = 0;
+        int digit = 0;
+        while (number !=0)
+        {
+
+           digit = number % 10 ;
+           reversed = reversed *10 + digit;
+           number = number / 10 ;
+            
+        }
+        System.err.println("THE REVERSED NUMBER IS "+ reversed);
+
+    }
+ }
