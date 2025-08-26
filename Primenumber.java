5dfafeaa1aae4e146117ef49e761ce4face3b05f
yashvanthbalaji <333yashvanthbalaji@gmail.com>
2025-08-26 10:59:42 +0530

    dayy 2 program  uploaded

---
+++ Primenumber.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Primenumber 
+{
+    public static void main (String []args)
+    {
+        Scanner scanner = new Scanner (System.in);
+        int num = scanner.nextInt();
+        int div_count = 0;
+        for (int i =1 ; i<= num; i++)
+        {
+            if (num%i==0)
+            {
+                div_count += 1;
+            }
+        }
+        System.out.println(div_count);
+        if (div_count == 2) {
+            System.out.println(num + "--It is Prime number");
+            
+        }
+        else{
+            System.out.println(num + "--It is not a Prime Number");
+        }
+    }
+
+}
