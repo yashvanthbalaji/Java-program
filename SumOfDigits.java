7c123230e603269ddeeaae7d50401913547b8894
yashvanthbalaji <333yashvanthbalaji@gmail.com>
2025-08-26 11:20:51 +0530

    Day 2 uploaded

---
+++ SumOfDigits.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class SumOfDigits
+{
+    public static void main (String []args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int digit=0;
+        int sum = 0;
+        int original = n ; 
+        while (n!=0)
+        {
+            digit = n%10;
+            sum = sum + digit ;
+            n = n/10;
+
+
+        }
+        System.out.println("Sum of digits of " + original + " is " + sum);
+    }
+}
